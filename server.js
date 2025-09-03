cb32a1c16da49d0a32fba62e8bdafb1c326efac0
DIPAKK2310 <dipakkhare217@gmail.com>
2025-09-03 18:38:34 +0000

    Added routes to server

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const server = http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type": "text/plain"}
 
     );
-
+    if (req.url ==="/") {
+        res.end("Welcome to 🏠 page")
+    } else if(req.url === "/about"){
+        res.end("This is about📟 page")
+    }else if (req.url === "/contact"){
+        res.end(`<h3>Contact to sunnyleone69@hotmail.com</h3>,Mobile no:696969`)
+    }else{
+        res.writeHead(404,{"Content-Type":"text/plain"})
     res.end("Hello from http server");
+    }
+
 });
 
 
