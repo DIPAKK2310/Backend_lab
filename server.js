35ef7952817fb7f656cb9857f130dd023c27d303
DIPAKK2310 <dipakkhare217@gmail.com>
2025-09-03 18:30:23 +0000

    Created http server

---
+++ server.js
@@ -0,0 +1,16 @@
+import http from "http"
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-Type": "text/plain"}
+
+    );
+
+    res.end("Hello from http server");
+});
+
+
+const PORT = 3000;
+
+server.listen(PORT,()=>{
+    console.log(`server is listnening on http://localhost:${PORT}`)
+})
